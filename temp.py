88f2e87fccb739652dfd4266f9d8c936fd9dd3e7
Nguyen Binh <hypixel2301@gmail.com>
2022-07-08 21:01:07 +0700

    change pytho file

--- temp.py
+++ temp.py
@@ -1,14 +1,1 @@
-def stringcut(s):
-    temps = []
-    ret = []
-    char = s[0]
-    for i in s:
-        if i == char:
-            temps.append(i)
-        else:
-            ret.append(temps)
-            temp = []
-            temp.append(i)
-            char = i
-    return ret
-print(stringcut("aaabbbccc"))
+print('hello world')
